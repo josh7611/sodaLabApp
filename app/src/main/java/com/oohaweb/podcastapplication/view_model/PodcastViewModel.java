24bf54f642008900522aac49ec8c40887c6a11c0
Josh <josh7611@gmail.com>
2018-11-10 13:07:24 +0800

    feat: Add podcast view model

--- app/src/main/java/com/oohaweb/podcastapplication/view_model/PodcastViewModel.java
+++ app/src/main/java/com/oohaweb/podcastapplication/view_model/PodcastViewModel.java
@@ -1,14 +1,21 @@
 package com.oohaweb.podcastapplication.view_model;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 import com.oohaweb.podcastapplication.model.Podcast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class PodcastViewModel extends ViewModel {
+    private static final String TAG = "PodcastViewModel";
     private static PodcastViewModel INSTANCE;
 
     private PodcastViewModel() {
@@ -24,8 +31,31 @@ public class PodcastViewModel extends ViewModel {
         return INSTANCE;
     }
 
-    public LiveData<List<Podcast>> getPodcasts() {
-        return null;
+    public LiveData<List<Podcast>> getPodcasts(Context context) {
+        final MutableLiveData<List<Podcast>> podcasts = new MutableLiveData<>();
+        podcasts.setValue(getFromMediaStore(context));
+        return podcasts;
+    }
+
+    private List<Podcast> getFromMediaStore(Context context) {
+        List<Podcast> podcasts = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    String songName = cursor.getString(cursor.
+                            getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    String path = cursor.getString(cursor
+                            .getColumnIndex(MediaStore.Audio.Media.DATA));
+
+                    podcasts.add(new Podcast(songName, path));
+                } while (cursor.moveToNext());
+            }
+        }
+        return podcasts;
     }
 
 }
